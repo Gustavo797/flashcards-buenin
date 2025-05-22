a11a7cec4b37a4a8b5e03ec706442f5baec4f797
Gustavo797 <alan.bueno.gustavo@escola.pr.gov.br>
2025-05-22 13:11:08 -0300

    Add files via upload

---
+++ flashcard- gustavo alan 2b/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  '  colheitadeiras',
+    'qual a colheitadeira mais cara do mundo?',
+    'A colheitadeira mais cara do mundo é a New Holland CR11'
+)
+
+criaCartao(
+    'Tratores',
+    'qual é o trator mais caro do mundo?',
+    'O trator mais caro do mundo, em termos de valor de transação, é o modelo 1913 Case 30 – 60'
+)
+
+criaCartao(
+    'pulverizadores',
+    'qual é o pulverizador mais caro do mundo?',
+    'O pulverizador autopropelido Jacto Uniport 4530 e 3030'
+)
+
+criaCartao(
+    'Fazendas',
+    'Qual a maior fazenda do mundo?',
+    'A maior fazenda do mundo é a Mudanjiang City Mega Farm, localizada na província de Heilongjiang, na China, com uma área de aproximadamente 22,5 milhões de acres (9,1 milhões de hectares)'
+)
